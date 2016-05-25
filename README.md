c0f457568e4c15e129d038cc4264c24bc6cc73e9
Dave Justice <davejustishh@gmail.com>
2016-05-25 19:38:43 -0400

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# audio-clock-webpack
+
+[![NPM](https://nodei.co/npm/audio-clock-webpack.png?downloads=true)](https://npmjs.org/package/audio-clock-webpack)
+
+same as [audio-clock](https://npmjs.org/package/audio-clock) except it
+uses
+[webworkify-webpack](https://github.com/borisirota/webworkify-webpack)
+instead of [webworkify](https://github.com/substack/webworkify)
---
+++ index.js
@@ -0,0 +1,58 @@
+var work = require('webworkify-webpack');
+var w = work(require('./werk.js'));
+
+var MINUTE_IN_MS = 60000;
+var currentBPM = {bpm: 120, ms: 500 / 4};
+
+function onTick(cb) {
+  w.addEventListener('message', function(ev) {
+    if (ev.data === 'tick') {
+      cb();
+    }
+  })
+}
+
+function setMS(ms) {
+  setCurrent(null, ms);
+  w.postMessage({interval: currentBPM.ms});
+}
+
+function setCurrent(bpm, ms) {
+  if (bpm) {
+    currentBPM = {
+      bpm: bpm,
+      ms: (MINUTE_IN_MS/bpm) / 4
+    };
+  } else {
+    currentBPM = {
+      bpm: MINUTE_IN_MS/ms,
+      ms: ms / 4
+    };
+  }
+}
+
+function start() {
+  w.postMessage('start');
+}
+
+function stop() {
+  w.postMessage('stop');
+}
+
+function setBPM(BPM) {
+  setCurrent(BPM);
+  w.postMessage({interval: currentBPM.ms});
+}
+
+function getBPM() {
+  return currentBPM;
+}
+
+module.exports = {
+  onTick: onTick,
+  setMS: setMS,
+  start: start,
+  stop: stop,
+  setBPM: setBPM,
+  getBPM: getBPM
+};
